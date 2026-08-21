5795a1644d301002e98b63635523009a19a62a9b
lesped <leospeder@gmail.com>
2026-08-21 14:41:16 +0200

    Delete Evol.dépenses_clients.sql

--- Evol.dépenses_clients.sql
+++
@@ -1,7 +0,0 @@
-Select Customer.CustomerId,
-Invoice.InvoiceId,
-Invoice.InvoiceDate as DATE_facture,
-Invoice.Total as CA_client,
-first_value (Invoice.Total) over(partition by Customer.CustomerId order by Invoice.InvoiceDate) as premiere_facture,
-(Invoice.Total) - (first_value (Invoice.Total) over(partition by Customer.CustomerId order by Invoice.InvoiceDate)) as difference
-FROM Customer join Invoice on Customer.CustomerId = Invoice.CustomerId ;
